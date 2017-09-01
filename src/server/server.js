ab40353c4d0830edc97098bba9aed2a519cf0151
Jeffrey Leu <jeffrey.leu@springboardauto.com>
2017-08-31 21:05:54 -0700

    Resolved passing props issue

--- src/server/server.js
+++ src/server/server.js
@@ -13,15 +13,15 @@ app.use(require('webpack-dev-middleware')(compiler, {
 }));
 app.use(require('webpack-hot-middleware')(compiler));
 
-// Redirect user back to index.html (used with react-router-dom)
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'client/index.html'));
-});
-
 // Render static index route
 app.use(express.static(path.join(__dirname, '../client')));
 
 // Routes
 app.get('/getCardData', getCardData);
+
+// Redirect user back to index.html (used with react-router-dom)
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'client/index.html'));
+});
 
 app.listen(port, () => { console.log(`server.js has been served on port: ${port}`); });
